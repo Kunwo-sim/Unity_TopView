809424efa8762d6eec91ebef4e621fe2ded6326c
SKW <rjsdntlsdj@gmail.com>
2021-11-15 23:52:35 +0900

    [Add] Player Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,76 @@ public class Player : MonoBehaviour
 {
     float h;
     float v;
-    int speed = 5;
+    bool isHorizonMove;
+    public float Speed = 2;
+    Animator Anim;
     Rigidbody2D rigid;
+    [SerializeField]
+    Vector2 dirVec;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if (hDown)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown)
+        {
+            isHorizonMove = false;
+        }
+        else if (hUp || vUp)
+        {
+            isHorizonMove = h != 0;
+        }
+
+        if(Anim.GetInteger("hAxisRaw") != h)
+        {
+            Anim.SetBool("isChange", true);
+            Anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (Anim.GetInteger("vAxisRaw") != v)
+        {
+            Anim.SetBool("isChange", true);
+            Anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            Anim.SetBool("isChange", false);
+        }
+
+        // 보고 있는 방향
+        if (vDown && v == 1)
+            dirVec = Vector2.up;
+        else if (vDown && v == -1)
+            dirVec = Vector2.down;
+        else if (hDown && h == 1)
+            dirVec = Vector2.right;
+        else if (hDown && h == -1)
+            dirVec = Vector2.left;
+
+        Debug.DrawRay(transform.position, dirVec, Color.red);
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, dirVec, 1.0f, LayerMask.GetMask("Object"));
+        if (rayHit.collider != null)
+        {
+            Debug.Log(rayHit.collider.gameObject.name);
+        }
     }
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h * speed, v * speed);
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed;
     }
 }
